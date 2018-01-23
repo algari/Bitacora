b8a091a48503a68237e52a3afc1fd5152f594a12
= <galcorp@gmail.com>
2018-01-23 17:29:18 -0500

    Se crean campos para cambio de password

--- src/app/private/users/change-password/change-password.component.ts
+++ src/app/private/users/change-password/change-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../../public/services/authentication.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UsersService} from "../../services/users.service";
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  edit:boolean = false;
+  isLoading = true;
+
+  constructor(
+    public _formBuilder: FormBuilder,
+    public _userS: UsersService,
+    public _authS: AuthenticationService,
+    public _router: Router,
+    public _activatedRoute: ActivatedRoute
+  ) { }
+
+  form = this._formBuilder.group( {
+    userx: this._formBuilder.group( {
+      password: [ ,Validators.required],
+      password1: [,[Validators.required, Validators.minLength(5)]],
+      password2: [,[Validators.required, Validators.minLength(5)]],
+    } )
+  } );
 
   ngOnInit() {
+    this.isLoading = false;
+  }
+
+  onSubmit() {
+
+
+  }
+
+  isRequired( fieldName: string ): boolean {
+    return this.form.get( `userx.${fieldName}` ).hasError( 'required' )
+      && this.form.get( `userx.${fieldName}` ).touched;
   }
 
 }
